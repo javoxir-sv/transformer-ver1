cf0c2f3581876f189382bec3083d95e0c765f982
Javokhir <javokhir.coding@gmail.com>
2026-09-09 19:20:46 +0500

    using torch.full

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+from numpy import dtype
 from torch.utils.data import Dataset
 import torch
 import torch.nn as nn
@@ -39,20 +40,23 @@ class BilingualDataset(Dataset):
             self.sos_token,
             torch.tensor(enc_input_tokens, dtype=torch.int64),
             self.eos_token,
-            torch.tensor([self.pad_token] * enc_num_pad_tokens, dtype=torch.int64)
-            ], dim=0)
+            torch.full((enc_num_pad_tokens,), self.pad_token.item(), dtype=torch.int64)
+            # torch.tensor([self.pad_token] * enc_num_pad_tokens, dtype=torch.int64)
+            ]) #, dim=0
 
         decoder_input = torch.cat([
             self.sos_token,
             torch.tensor(dec_input_tokens, dtype=torch.int64),
-            torch.tensor([self.pad_token] * dec_num_pad_tokens, dtype=torch.int64)
-            ], dim=0)
+            torch.full((enc_num_pad_tokens,), self.pad_token.item(), dtype=torch.int64)
+            # torch.tensor([self.pad_token] * dec_num_pad_tokens, dtype=torch.int64)
+            ]) #, dim=0 
 
         label = torch.cat([
             torch.tensor(enc_input_tokens, dtype=torch.int64),
             self.eos_token,
-            torch.tensor([self.pad_token] * enc_num_pad_tokens, dtype=torch.int64)
-            ], dim=0)
+            torch.full((enc_num_pad_tokens,), self.pad_token.item(), dtype=torch.int64)
+            # torch.tensor([self.pad_token] * enc_num_pad_tokens, dtype=torch.int64)
+            ]) #, dim=0
 
         # length check
         assert encoder_input.size(0) == self.seq_len, "encoder_input != seq_len"
